8f618d1d9b414be38d317a4f35187c199eb75aa8
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-07 23:44:27 +0100

    Moved preference tests as well.

---
+++ src/test/Test.Yaaf.MessageArchiveManager/PreferenceStoreTests.fs
@@ -0,0 +1,49 @@
+﻿// ----------------------------------------------------------------------------
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ----------------------------------------------------------------------------
+namespace Test.Yaaf.MessageArchiveManager
+
+open FsUnit
+open NUnit.Framework
+open Yaaf.Helper
+open Yaaf.TestHelper
+
+open Test.Yaaf.Xmpp
+open Yaaf.Xmpp
+open Yaaf.Xmpp.IM.Server
+open Yaaf.Xmpp.IM
+open Swensen.Unquote
+open Foq
+open Yaaf.Xmpp.MessageArchiving
+
+[<AbstractClass>]
+type PreferenceStoreTests() =
+    inherit MyTestClass()
+
+    let defUser = (JabberId.Parse "user@nunit.org")
+    abstract CreatePreferenceStore: JabberId -> IUserPreferenceStore
+
+    override x.Setup () = 
+        base.Setup()
+
+    override x.TearDown() = 
+        base.TearDown()
+        
+        
+    [<Test>]
+    member x.``Check that storing and deleting default preferences works``() =
+        let prefStore = x.CreatePreferenceStore defUser
+        test <@ prefStore.GetUserPreferences () |> waitTask = None @>
+
+        prefStore.SetDefaultOtrSaveMode (Some { Expire = Some 123L; OtrMode = Some OtrMode.Concede; SaveMode = Some SaveMode.Body }) |> Task.ofPlainTask |> waitTask
+        let results = prefStore.GetUserPreferences () |> waitTask
+        test <@ results.IsSome @>
+        let saveMode = results.Value.DefaultOtrSaveMode
+        test <@ saveMode.Expire = Some 123L @>
+        test <@ saveMode.OtrMode = Some OtrMode.Concede @>
+        test <@ saveMode.SaveMode = Some SaveMode.Body @>
+        prefStore.SetDefaultOtrSaveMode None |> Task.ofPlainTask |> waitTask
+
+        let results = prefStore.GetUserPreferences () |> waitTask
+        test <@ results.IsNone @>
