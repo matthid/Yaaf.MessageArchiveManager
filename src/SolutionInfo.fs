e8dfcfd2dd8f5b259f5e0db7c047271eff190bd6
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-09 15:04:54 +0100

    Bump version to 0.0.2

--- src/SolutionInfo.fs
+++ src/SolutionInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.MessageArchiveManager")>]
 [<assembly: AssemblyProductAttribute("Yaaf.MessageArchiveManager")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.MessageArchiveManager Copyright © Matthias Dittrich 2015")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
